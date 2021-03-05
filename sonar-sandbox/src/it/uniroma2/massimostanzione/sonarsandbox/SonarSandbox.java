295a23cfda7b6390c8a3ea7da9e8ab210cd7d8e4
massimostanzione <massimo.stanzione@hotmail.it>
2021-03-05 23:46:34 +0100

    fix #1

--- sonar-sandbox/src/it/uniroma2/massimostanzione/sonarsandbox/SonarSandbox.java
+++ sonar-sandbox/src/it/uniroma2/massimostanzione/sonarsandbox/SonarSandbox.java
@@ -1,10 +1,7 @@
 package it.uniroma2.massimostanzione.sonarsandbox;
 
 public class SonarSandbox {
-
 	public static void main(String[] args) {
-	
-
+		System.out.println("Hello SonarCloud!");
 	}
-
 }
