3f39be514a5ff54d5f5d42bc0a29a5723bef96fd
massimostanzione <massimo.stanzione@hotmail.it>
2021-03-08 21:51:00 +0100

    +validazione

--- src/it/uniroma2/massimostanzione/sonarsandbox/SonarSandbox.java
+++ src/it/uniroma2/massimostanzione/sonarsandbox/SonarSandbox.java
@@ -4,11 +4,13 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class SonarSandbox {
+	private static int var=7;
 	public static void main(String[] args) {
 		Logger logger = Logger.getLogger("global");
-		logger.log(Level.INFO, "Hello SonarCloud! Da Debian!");
-		logger.log(Level.INFO, "... e pure da Windows!");
-		logger.log(Level.INFO, "Ma senza il ghost?");
-		logger.log(Level.INFO, "Sì, senza il ghost e pure da Debian, ma lo scanner non gira.");
+		logger.log(Level.CONFIG, "Hello SonarCloud! Da Debian!");
+		logger.log(Level.CONFIG, "... e pure da Windows!");
+		logger.log(Level.WARNING, "Ma senza il ghost?");
+		logger.log(Level.CONFIG, "Sì, senza il ghost e pure da Debian, ma lo scanner non gira.");
+		logger.log(Level.INFO, "E la variabile vale "+var);
 	}
 }
