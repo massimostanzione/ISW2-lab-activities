0a40bf3b6a910871cc0441c75d1e2ff165abb392
Massimo Stanzione <Massimo@192.168.1.18>
2021-03-08 19:01:58 +0100

    da windows

--- src/it/uniroma2/massimostanzione/sonarsandbox/SonarSandbox.java
+++ src/it/uniroma2/massimostanzione/sonarsandbox/SonarSandbox.java
@@ -3,5 +3,6 @@ package it.uniroma2.massimostanzione.sonarsandbox;
 public class SonarSandbox {
 	public static void main(String[] args) {
 		System.out.println("Hello SonarCloud! Da Debian!");
+		System.out.println("... e pure da Windows!");
 	}
 }
