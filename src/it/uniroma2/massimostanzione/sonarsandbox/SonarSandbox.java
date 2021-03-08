53f4ebf4b57c35e474bee281722414e1eda98f5f
massimostanzione <massimo.stanzione@hotmail.it>
2021-03-08 19:16:29 +0100

    fix #2 (si spera)

--- src/it/uniroma2/massimostanzione/sonarsandbox/SonarSandbox.java
+++ src/it/uniroma2/massimostanzione/sonarsandbox/SonarSandbox.java
@@ -1,9 +1,13 @@
 package it.uniroma2.massimostanzione.sonarsandbox;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class SonarSandbox {
 	public static void main(String[] args) {
-		System.out.println("Hello SonarCloud! Da Debian!");
-		System.out.println("... e pure da Windows!");
-		System.out.println("Ma senza il ghost?");
+		Logger logger = Logger.getLogger("global");
+		logger.log(Level.INFO, "Hello SonarCloud! Da Debian!");
+		logger.log(Level.INFO, "... e pure da Windows!");
+		logger.log(Level.INFO, "Ma senza il ghost?");
 	}
 }
